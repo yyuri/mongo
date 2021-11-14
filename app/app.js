7914407f62ce8af426afec7ecaaff368f425260a
Oriol <o.goma@inpr3mium.com>
2021-11-14 13:11:41 +0100

    Added documentation with Swagger and static server

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ const helmet = require('helmet')
 const cors = require('cors')
 const passport = require('passport')
 
+// Swagger API documentation
+const swaggerDocument = require('./static/swagger.json');
+const swaggerUi = require('swagger-ui-express');
+
 // app imports
 const { connectToDatabase } = require("./config/mongo");
 const { errorHandler } = require("./handlers");
@@ -37,12 +41,19 @@ app.use(passport.initialize())
 
 // Load and use all routes
 app.use("/", router)
+
+const uiDocsOptions = {
+  customCssUrl: '/static/style.css'
+};
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, uiDocsOptions));
+app.use('/static', express.static('./app/static'));
 // catch-all for 404 "Not Found" errors
 app.get("*", fourOhFourHandler);
 // catch-all for 405 "Method Not Allowed" errors
 app.all("*", fourOhFiveHandler);
-
 app.use(globalErrorHandler);
+
 
 /**
  * This file does NOT run the app. It merely builds and configures it then exports it.config
